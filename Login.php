1f9df2a397da36392b271e05d6f3e2a84c64e929
Richard Vullings <rvullings8@gmail.com>
2022-05-10 13:57:33 +0200

    Clean #1
    include, login

---
+++ Login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Richard Vullings">
+    <title>Login</title>
+</head>
+<body>
+    <form action="login.php" method="POST">
+        <input type="email" name="logmail" placeholder="E-Mail" id="email">
+        <input type="password" name="logpassword" id="password" placeholder="Password">
+        <input id='show'type="checkbox">
+    <input type="submit" value="Send" name="submit">
+</form>
+</body>
+</html>
