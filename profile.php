9a1cb9cfe9a9b293b469272e74fa4217b15f5390
Richard Vullings <rvullings8@gmail.com>
2022-05-11 14:28:57 +0200

    fix?

--- profile.php
+++ profile.php
@@ -13,21 +13,14 @@
 
 ?>
 <script>
-    function performClick(elemId) {
+  function performClick(elemId) {
     var elem = document.getElementById(elemId);
     if(elem && document.createEvent) {
        var evt = document.createEvent("MouseEvents");
        evt.initEvent("click", true, false);
        elem.dispatchEvent(evt);
     }
-}
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
+  }
 </script>
 
 <!DOCTYPE html>
@@ -48,38 +41,27 @@ function closeForm() {
         </a>
         <input type="file" id="image-input" accept="image/jpeg, image/png, image/jpg">
     </div>
-    <div class="form-popup" id="myForm">
-  <form action="/action_page.php" class="form-container">
-    <h1>Login</h1>
 
-    <label for="email"><b>Old Password</b></label>
-    <input type="text" placeholder="Old Password" name="psw" required>
-
-    <label for="psw"><b>New Password</b></label>
-    <input type="password" placeholder="New Password" name="psw" required>
-    
-    <label for="psw"><b>Confirm New Password</b></label>
-    <input type="password" placeholder="Confirm New Password" name="psw" required>
-
-    <button type="submit" class="btn">Confirm Change</button>
-    <button type="button" class="btn cancel" onclick="">Cancel</button>
-  </form>
-</div>
     <div class="Settings">
-        <div class="Information">
+
+      <div class="Information">
         <h2>Information</h2> 
         <!-- iemand die back end doet moet even zorgen dat de correcte text hier komt te staan -Stan -->
         <h5>E-mail</h5>
         <h5>Password</h5>
         <h5>Account Age</h5>
         <h2>Settings</h2>
+
         <form method="POST" action="profile.php">
-        <input id="button" type="submit" name="btnChangePass" value="Change Password" onclick="openForm()">
-        <input id="button" type="submit" name="btnDeleteAcc" value="Delete Account" onclick="Delete()">
-        <input id="button" type="submit" name="btnChangePfp" value="Change Profile Picture">
+
+          <input class="btnPfPage" type="submit" name="btnChangePass" value="Change Password" onclick="openForm()">
+          <input class="btnPfPage" type="submit" name="btnDeleteAcc" value="Delete Account" onclick="Delete()">
+          <input class="btnPfPage" type="submit" name="btnChangePfp" value="Change Profile Picture">
+
         </form>
-</div>
-</div>
+
+      </div>
+    </div>
 </body>
 </html>
 
