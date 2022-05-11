55c686db64127667362543563b3b86275c56ee04
Richard Vullings <rvullings8@gmail.com>
2022-05-11 13:49:55 +0200

    fix alles css

--- inc/functions.php
+++ inc/functions.php
@@ -53,54 +53,60 @@ function log_in(){
 ///////////
 //Nav Bar//
 //////////
-function navBar(){
-    ?>
-    <div class="pixelnav"> <!-- nav -->
-    <div class="navbar">
-
-        <a href="index.php">Home</a>
-        <a href="">About</a>
-        <a href="Contact">Contact</a>
-
-        <div class="dropdown">
-          <button class="dropbtn">Categories
-            <i class="fa fa-caret-down"></i>
-          </button>
-          <div class="dropdown-content">
-            <div class="header">
-              <h2>Mega Menu</h2>
-            </div>
-            <div class="row">
-              <div class="column">
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"> <h3>NFT</h3></a>
-              </div>
-              <div class="column">
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"><h3>Electronics</h3></a>
-              </div>
-              <div class="column">
-                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"><h3>Pokemon Cards</h3></a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <?php
-        if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true){
-            ?>
-            <a href="logout.php">Logout</a>
-            <a href=""><?=$_SESSION['currency']?></a> 
-            <img href="profile.php"id="logo" src="images/pixel trading.png">
-            <?php
-        }
-        else{
-            ?>
-            
-            <a href="Login.php">Login</a>
-            <a href="create_acc.php">Make Account</a>
-            <?php
-        }
-        ?> 
+function dropdown(){
+  ?>
+  <div class="dropdown">
+  <button class="dropbtn">Categories
+    <i class="fa fa-caret-down"></i>
+  </button>
+  <div class="dropdown-content">
+    <div class="header">
+      <h2>Mega Menu</h2>
+    </div>
+    <div class="row">
+      <div class="column">
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"> <h3>NFT</h3></a>
+      </div>
+      <div class="column">
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"><h3>Electronics</h3></a>
+      </div>
+      <div class="column">
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"><h3>Pokemon Cards</h3></a>
       </div>
     </div>
+  </div>
+</div>
+<?php
+}
+function navBar(){
+    ?>
+     <!-- nav -->
+    <div class="navbar">
+
+        <a class="navbar-content" href="index.php">Home</a>
+        <a class="navbar-content" href="">About</a>
+        <a class="navbar-content" href="Contact">Contact</a>
+
+        <?php 
+          dropdown();
+
+          if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true){
+            ?>
+              <a class="navbar-content" href="logout.php">Logout</a>
+              <a class="navbar-content" href=""><?=$_SESSION['currency']?></a> 
+              <a href="profile.php"><img href="profile.php"id="logo" src="images/pixel trading.png"></a> 
+            <?php
+          }
+          else{
+              ?>
+              
+              <a class="navbar-content" href="Login.php">Login</a>
+              <a class="navbar-content" href="create_acc.php">Make Account</a>
+              <?php
+          }
+        ?> 
+      </div>
+    
     <?php
 }
 
