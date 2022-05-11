92aaff91725482f8c22b4f8c2f0355222ddededf
STANdaard05 <stanvogels158@gmail.com>
2022-05-11 09:34:44 +0200

    Profile
    profile updated

--- profile.php
+++ profile.php
@@ -5,12 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
 </head>
 <body>
     <h1>Profile</h1>
-    <div class="profilepic">
-        <h2>Change Profile Picture</h2>
-        <img src="images/blank-profile-picture-973460_1280.png" width="200" height="200">
+    <div class="profile">
+        <img class="profilepic" src="images/blank-profile-picture-973460_1280.png" width="255" height="255">
     </div>
+    <div class="Settings">
+        <h2>Information</h2>
+        <h5>E-mail</h5>
+        <h5>Password</h5>
+        <h5>Account Age</h5>
+        <h2>Settings</h2>
+        <button id="button" type="button">Change Password</button>
+        <button id="button" type="button">Delete Account</button>
+        <button id="button" type="button">Change Profile Picture</button>
+</div>
 </body>
 </html>
