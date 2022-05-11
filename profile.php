954e5c910d84b351829af896e67b2d3071a52fee
Fabiwell <80628214+Fabiwell@users.noreply.github.com>
2022-05-11 10:25:57 +0200

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,14 +1,3 @@
-<script>
-    function performClick(elemId) {
-    var elem = document.getElementById(elemId);
-    if(elem && document.createEvent) {
-       var evt = document.createEvent("MouseEvents");
-       evt.initEvent("click", true, false);
-       elem.dispatchEvent(evt);
-    }
- }
-</script>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,10 +11,10 @@
     <h1>Profile</h1>
     <div class="profile">
         <h2>Change Profile Picture</h2>
-        <a href="#" onclick="performClick('image-input');">
+        <a href="#">
             <img class="profilepic" src="images/blank-profile-picture-973460_1280.png" width="255" height="255">
         </a>
-        <input type="file" id="image-input" accept="image/jpeg, image/png, image/jpg">
+        <input type="file" id="image-input" accept="image/jpeg, image/png, image/jpg" style="display:none">
     </div>
     <div class="Settings">
         <h2>Information</h2>
@@ -35,7 +24,7 @@
         <h2>Settings</h2>
         <button id="button" type="button">Change Password</button>
         <button id="button" type="button">Delete Account</button>
-        <button id="button" type="button">Change Profile Picture</button>
+        <button id="button" type="button" onclick="changepfp()">Change Profile Picture</button>
 </div>
 </body>
 </html>
