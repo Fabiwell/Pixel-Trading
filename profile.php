6bc9068c5287d3e2f7f6867eb1df3f8d45309ed3
Fabiwell <80628214+Fabiwell@users.noreply.github.com>
2022-05-12 09:03:31 +0200

    Revert "Merge pull request #22 from Fabiwell/fabian-temp"
    This reverts commit 024eecb8c8c81c267a7157437f33030cd14473c3, reversing
    changes made to 5377e2c094fbed3aae60420433767ff99e910def.

--- profile.php
+++ profile.php
@@ -1,60 +1,26 @@
 <?php
-  include('inc/functions.php');
+    include('inc/functions.php');
+ 
+    if (isset($_POST["btnDeleteAcc"])) 
+    {
+        unset($_POST["btnDeleteAcc"]);
 
-  if (isset($_POST["btnDeleteAcc"])) 
-  {
-      unset($_POST["btnDeleteAcc"]);
+        // Code that only executes when submit is pressed
+        deleteAcc();
 
-      // Code that only executes when submit is pressed
-      deleteAcc();
-
-      header("Refresh:0");
-  }
-
-  $msg = "";
-  $db = mysqli_connect("localhost", "root", "", "pixel_trading");
-  // If upload button is clicked ...
-  if (isset($_POST['upload'])) {
-
-    $filename = $_FILES["image-input"]["name"];
-    $tempname = $_FILES["image-input"]["tmp_name"];   
-        $folder = "upload/".$filename;
-        
-    
-
-        // Get all the submitted data from the form
-        $sql = "INSERT INTO upload (filename) VALUES ('$filename')";
-
-        // Execute query
-        mysqli_query($db, $sql);
-        
-        // Now let's move the uploaded image into the folder: image
-        if (move_uploaded_file($tempname, $folder))  {
-            $msg = "Image uploaded successfully";
-        }else{
-            $msg = "Failed to upload image";
-      }
-  }
-  $result = mysqli_query($db, "SELECT * FROM upload");
+        header("Refresh:0");
+    }
 
 ?>
 <script>
-
-//     function performClick(elemId) {
-//     var elem = document.getElementById(elemId);
-//     if(elem && document.createEvent) {
-//        var evt = document.createEvent("MouseEvents");
-//        evt.initEvent("click", true, false);
-//        elem.dispatchEvent(evt);
-//     }
-// }
-
-if(document. getElementById('buttonupload'). clicked == true)
-{
-  document.getElementById("profilepic").src="upload/<?php echo($filename)?>";
+  function performClick(elemId) {
+    var elem = document.getElementById(elemId);
+    if(elem && document.createEvent) {
+       var evt = document.createEvent("MouseEvents");
+       evt.initEvent("click", true, false);
+       elem.dispatchEvent(evt);
+    }
 }
-
-
 function openForm() {
   document.getElementById("myForm").style.display = "block"
   event.preventDefault();
@@ -95,19 +61,16 @@ function closeForm() {
 
         <div class="profile">
         <h2>Change Profile Picture</h2>
+        <a href="#" onclick="performClick('image-input');">
+            <img class="profilepic" src="images/blank-profile-picture-973460_1280.png" width="255" height="255">
+        </a>
+        
+        </div>
 
-        <img id="profilepic" src="images/blank-profile-picture-973460_1280.png" width="255" height="255">
-    </div>
-    <div class="form-popup" id="myForm">
-  <form action="/action_page.php" class="form-container">
-    <h1>Login</h1>
-
-
-        <form method="POST" action="profile.php" enctype="multipart/form-data">
+        <form method="POST" action="profile.php">
 
           <input class="btnPfPage" type="submit" name="btnChangePass" value="Change Password" onclick="openForm()">
           <input class="btnPfPage" type="submit" name="btnDeleteAcc" value="Delete Account" onclick="Delete()">
-          <input id="buttonupload" type="submit" name="upload" value="Change Profile Picture">()
           <input type="file" id="image-input" accept="image/jpeg, image/png, image/jpg">
 
         </form>
